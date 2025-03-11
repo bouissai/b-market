d2e3ec8e5314a28343ac724b172b6af2a3b4bba8
bouissai <bouissailyass@gmail.com>
2025-03-11 19:32:37 +0100

    refacto

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ const nextConfig = {
   productionBrowserSourceMaps: true, // Active les source maps en prod
   compress: true, // Active la compression Gzip/Brotli
   images: {
-    domains: ["images.unsplash.com"],
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
