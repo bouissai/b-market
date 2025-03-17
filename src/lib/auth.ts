f59fa878aa244a0d657ab823058454ded1668df8
flavien <flaviensuman@gmail.com>
2025-03-17 18:48:04 +0100

    initialisation de better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,10 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import {prisma} from "@/lib/prisma";
 
+
+
 export const auth = betterAuth({
+    emailAndPassword: {enabled: true},
     database: prismaAdapter(prisma, {
         provider: "sqlite", // or "mysql", "postgresql", ...etc
     }),
