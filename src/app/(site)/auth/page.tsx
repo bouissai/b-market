d87340fe038ece5c8c8b7d657b187d5e02b65c64
bouissai <bouissailyass@gmail.com>
2025-03-23 15:49:41 +0100

    fix bug sur le  toast qui ne s'affiche pas la premiere fois

--- src/app/(site)/auth/page.tsx
+++ src/app/(site)/auth/page.tsx
@@ -1,37 +1,51 @@
-'use client';
+"use client"
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import SignIn from '@/components/user/auth/sign-in';
-import SignUp from '@/components/user/auth/sign-up';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import SignIn from "@/components/user/auth/sign-in"
+import SignUp from "@/components/user/auth/sign-up"
+import { useAuthStore } from "@/store/useAuthStore"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 
 const Auth = () => {
-	const { data: session } = useSession();
-	const router = useRouter();
-	const [tab, setTab] = useState('sign-in');
-	useEffect(() => {
-		if (session) {
-			router.replace('/');
-		}
-	}, [session, router]);
-	return (
-		<div className="flex justify-center items-center h-[calc(100vh-249px-80px)]">
-			<Tabs value={tab} onValueChange={setTab} defaultValue="sign-in" className=" w-[400px]">
-				<TabsList className="grid w-full grid-cols-2">
-					<TabsTrigger value="sign-in">Connexion</TabsTrigger>
-					<TabsTrigger value="sign-up">Inscription</TabsTrigger>
-				</TabsList>
-				<TabsContent value="sign-in">
-					<SignIn />
-				</TabsContent>
-				<TabsContent value="sign-up">
-				<SignUp onSuccess={() => setTab('sign-in')} />
-				</TabsContent>
-			</Tabs>
-		</div>
-	);
-};
+  const { data: session } = useSession()
+  const router = useRouter()
+  const [tab, setTab] = useState("sign-in")
+  const { setError } = useAuthStore()
 
-export default Auth;
+
+  useEffect(() => {
+    if (session) {
+      router.replace("/")
+    }
+  }, [session, router])
+
+  return (
+    <div className="flex justify-center items-center py-8 min-h-[50vh]">
+      <Tabs
+        value={tab}
+        onValueChange={(newTab) => {
+          setError(null);
+          setTab(newTab);
+        }}
+        defaultValue="sign-in"
+        className="w-full max-w-[400px] mx-auto"
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="sign-in">Connexion</TabsTrigger>
+          <TabsTrigger value="sign-up">Inscription</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <SignIn />
+        </TabsContent>
+        <TabsContent value="sign-up">
+          <SignUp onSuccess={() => setTab("sign-in")} />
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
+
+export default Auth
+
