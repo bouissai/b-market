53bcfed077f5eae5287ed5e316770b374bdc25ff
Flavien <flaviensuman@gmail.com>
2025-05-27 12:05:05 +0200

    code promo reset quand quitte checkout

--- src/app/(site)/checkout/page.tsx
+++ src/app/(site)/checkout/page.tsx
@@ -6,17 +6,20 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 import Checkout from '@/components/site/checkout/checkout';
 import { useCheckoutStore } from '@/store/useCheckoutStore';
+import { usePromoCodeStore } from '@/store/usePromoCodeStore';
 
 export default function CheckoutPage() {
 	const { data: session, status } = useSession();
 	const router = useRouter();
 	const setRedirectPath = useAuthStore(state => state.setRedirectPath);
 	const { resetCheckout } = useCheckoutStore();
+	const { clearPromoCode } = usePromoCodeStore();
 
 	useEffect(() => {
 		// Réinitialiser le checkout quand le composant est démonté
 		return () => {
 			resetCheckout();
+			clearPromoCode();
 		};
 	}, [resetCheckout]);
 
